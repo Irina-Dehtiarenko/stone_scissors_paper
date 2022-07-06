43f5bd94003059a079a4abeddd11d676a53cd0a4
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2022-07-06 16:29:10 +0200

    aded a function whitch return a random computer choise

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const options = document.querySelectorAll("img");
+const btnStartGame = document.querySelector("button");
+
+let game = {
+  playerChoice: "",
+  aiChoise: "",
+};
 
 const playerSelection = (e) => {
   const img = e.target;
@@ -7,9 +13,20 @@ const playerSelection = (e) => {
   });
   img.classList.add("active");
 
-  const playerChoice = img.dataset.option;
+  game.playerChoice = img.dataset.option;
+};
+
+const aiSelection = () => {
+  const index = Math.floor(Math.random() * options.length);
+  return options[index].dataset.option;
+};
+
+const startGame = (e) => {
+  game.aiChoise = aiSelection();
 };
 
 options.forEach((option) => {
   option.addEventListener("click", playerSelection);
 });
+
+btnStartGame.addEventListener("click", startGame);
